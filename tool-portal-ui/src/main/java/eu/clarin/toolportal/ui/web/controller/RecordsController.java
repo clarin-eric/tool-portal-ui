236ddf71fea6547c0a026cb0d99c1bd392bf1fb1
Twan Goosen <twan@clarin.eu>
2025-05-12 13:21:37 +0300

    Simple record page

---
+++ tool-portal-ui/src/main/java/eu/clarin/toolportal/ui/web/controller/RecordsController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2025 CLARIN ERIC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.clarin.toolportal.ui.web.controller;
+
+import eu.clarin.cmdi.vlo.openapi.client.model.VloRecord;
+import eu.clarin.toolportal.ui.service.RecordsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author twagoo
+ */
+@Controller
+@RequestMapping(value = "/records")
+public class RecordsController {
+
+    private final RecordsService service;
+
+    public RecordsController(RecordsService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{recordId}")
+    public String record(Model model,
+            @PathVariable String recordId) {
+        final VloRecord record = service.getRecordById(recordId);
+        model.addAttribute("record", record);
+
+        return "/records/record";
+    }
+
+}
