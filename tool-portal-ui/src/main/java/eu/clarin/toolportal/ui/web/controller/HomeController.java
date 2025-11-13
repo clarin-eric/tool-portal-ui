642512bd13181487b738a62b27fabea9e28a65b9
Twan Goosen <twan@clarin.eu>
2025-11-13 18:39:34 +0200

    Highlighted records loaded and rendered into home page

--- tool-portal-ui/src/main/java/eu/clarin/toolportal/ui/web/controller/HomeController.java
+++ tool-portal-ui/src/main/java/eu/clarin/toolportal/ui/web/controller/HomeController.java
@@ -16,10 +16,16 @@
  */
 package eu.clarin.toolportal.ui.web.controller;
 
-import com.google.common.collect.ImmutableList;
+import eu.clarin.cmdi.vlo.openapi.client.model.VloRecord;
+import eu.clarin.toolportal.ui.service.RecordsService;
+import eu.clarin.toolportal.ui.service.filter.RecordFilter;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,8 +37,41 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class HomeController {
 
-    @GetMapping
-    public List<ModelAndView> index() {
-        return ImmutableList.of(new ModelAndView("home/home"));
+    private final RecordsService service;
+
+    private final RecordFilter recordFilter;
+
+    public HomeController(RecordsService service, RecordFilter recordFilter) {
+        this.service = service;
+        this.recordFilter = recordFilter;
+    }
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView index(Model model) {
+        model.addAttribute("highlightedIds", List.of(
+                "CSD_32_Tools_47_WebCelex.cmdi.xml",
+                "https_58__47__47_tool-portal.clarin.eu_47_metadata_47_codemeta_47_galahad-train-battery_1.1.0.json",
+                "https_58__47__47_tool-portal.clarin.eu_47_metadata_47_switchboard_47_ELAN.json"));
+
+        return new ModelAndView("home/home");
+    }
+
+    @GetMapping(path = "/home/highlighted/{recordId}", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView highlighted(@PathVariable String recordId, Model model) {
+        final VloRecord record = RecordsService.applyFilter(
+                service.getRecordById(recordId),
+                recordFilter);
+
+        final Map<String, List<Object>> fields = record.getFields();
+
+        model.addAttribute("id", recordId);
+        if (fields != null) {
+            //TODO: extract title from list
+            model.addAttribute("title", fields.get("name"));
+            //TODO: truncate description
+            model.addAttribute("description", fields.get("description"));
+        }
+
+        return new ModelAndView("/home/highlighted :: highlight");
     }
 }
